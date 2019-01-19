b30e2623ec1d7aac26dac4caa667c6194f05f836
Matheus E. Muller <hello@memuller.com>
2019-01-19 18:20:51 -0200

    Uses hookable interface for UI

--- helpers/InterfaceHooks.php
+++ helpers/InterfaceHooks.php
@@ -1,37 +1,43 @@
 <?php
 namespace Appacademia;
 
-class InterfaceHooks {
+class InterfaceHooks
+{
 
-    static $hide_menus = [ 'Posts', 'Comments', 'Pages' ];
+  use \Bricks\Hookable;
 
-    static function init(){
-      static::enqueue_admin_assets();
-      static::hide_menus();
+  static $hide_menus = ['Posts', 'Comments', 'Pages'];
+
+  static $actions = [
+    'admin_menu' => 'hide_menus',
+    'admin_enqueue_scripts' => 'enqueue_admin_assets'
+  ];
+
+  static function init()
+  {
+    static::set_hooks();
+  }
+
+  static function hide_menus()
+  {
+    global $menu;
+    $restricted_menus = static::$hide_menus;
+    $restricted = array();
+    foreach ($restricted_menus as $item) {
+      $restricted[] = __($item);
     }
-
-    static function hide_menus(){
-      add_action('admin_menu', function () use ($class) {
-        global $menu;
-        $restricted_menus = static::$hide_menus;
-        $restricted = array();
-        foreach ($restricted_menus as $item) {
-          $restricted[]= __($item);
-        }
-        end ($menu);
-        while (prev($menu)){
-          $value = explode(' ',$menu[key($menu)][0]);
-          if(in_array($value[0] != NULL?$value[0]:"", $restricted)){
-            unset($menu[key($menu)]);
-          }
-        }
-      });
-    }
-
-    static function enqueue_admin_assets () {
-      add_action('admin_enqueue_scripts', function($hook){
-        wp_enqueue_style('bricks_admin_css',BRICKS_BASE_URL.'/assets/css/admin.css');
-        wp_enqueue_script('bricks_admin_script', BRICKS_BASE_URL.'/assets/js/admin.js');
-      });
+    end($menu);
+    while (prev($menu)) {
+      $value = explode(' ', $menu[key($menu)][0]);
+      if (in_array($value[0] != null ? $value[0] : "", $restricted)) {
+        unset($menu[key($menu)]);
+      }
     }
   }
+
+  static function enqueue_admin_assets()
+  {
+    wp_enqueue_style('bricks_admin_css', BRICKS_BASE_URL . '/assets/css/admin.css');
+    wp_enqueue_script('bricks_admin_script', BRICKS_BASE_URL . '/assets/js/admin.js');
+  }
+}
